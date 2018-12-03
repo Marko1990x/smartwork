613fc1e398c6be802126a84615c81cc1f420c37b
Marko Dunović <marko_dunovic@hotmail.com>
2018-12-03 17:40:58 +0100

    update
    dodat je rad na casu 1 za java script

---
+++ javascriptwork/rad na casu 1/radnacasu.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+ var a = ime;
+ var b = 5;
+ 
+ // ovo je test
+ 
+ // globalna promenjiva je kada se izbaci samo izvan funkcije
+ // -- pogledaj da li postoji lkjucna rec da se fixira promenjiva posle poziva funkcije
+ 
+ // text se pise unutar '' ili "" navodnika
